a1f6c8be5b420220975dc9c81cf7949e2836d774
Ashutosh <bhardwajashu96@hotmail.com>
2017-01-12 13:19:22 +0000

    made first working version

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var response = {};
+
+app.get('/*', function (req, res) {
+  var path = req.path;
+  // removing unicode character's from string
+  path = path.replace('%20',' ').replace('%20',' ').replace('/','');
+  // parsing String as Date
+  var unix = Date.parse(path);
+  // if a vaild natural date
+  if (!isNaN(unix)) {
+
+      var natural = new Date(unix);
+      natural = natural.toDateString();
+      natural = natural.slice(4);
+      // set JSON response
+      response.unix = unix;
+      response.natural = natural;
+  }
+  // else if valid unix date
+  else if(!isNaN(Number(path)) && Number(path) !== 0) {
+    // 
+    unix = path;
+    natural = new Date(Number(path));
+    natural = natural.toDateString();
+    natural = natural.slice(4);
+    // set JSON response
+    response.unix = unix;
+    response.natural = natural;
+  }
+  // anything else
+  else {
+    // set JSON response
+    response.unix = null;
+    response.natural = null;
+  }
+  // sending the JSON response
+  res.json(response);
 });
 
 app.listen(8080, function () {
